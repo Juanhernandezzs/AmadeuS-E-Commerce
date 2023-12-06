493c3650ef60df73c2e9fe8606d939223c1d2178
Juanhernandezzs <juanmhdz99@gmail.com>
2023-12-06 14:12:23 -0300

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -19,7 +19,7 @@ server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', `${SERVER}`); // update to match the domain you will make the request from (http://musical-e-commerce.vercel.app)
+  res.header('Access-Control-Allow-Origin', `*`); // update to match the domain you will make the request from (http://musical-e-commerce.vercel.app)
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
